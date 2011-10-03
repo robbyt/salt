d7c57993602ddde43af81cacc66c30033547fe42
Rob Terhaar <rob@atlanticdynamic.com>
2011-10-02 23:13:38 -0400

    first unit test

--- tests/salt_tests.py
+++ tests/salt_tests.py
@@ -1,6 +1,14 @@
+import os, random
 from nose.tools import *
 from salt import salt
 
+def clean_up_dir(d):
+    if os.path.isdir(d): os.removedirs(d)
+
 def verify_env_test():
-#    pass
-    salt.verify_env('tmp')
+    test_file = ['/tmp/' + str(random.getrandbits(10))]
+    clean_up_dir(test_file[0])
+    salt.verify_env(test_file)
+    assert_equal(os.path.isdir(test_file[0]), True)
+    clean_up_dir(test_file[0])
+
